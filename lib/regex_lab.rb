9d9ee2975f40139c89bc8452deda97bd61c8b384
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:33:58 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\A[aeiou]/)
+  if word.scan(/\w+\A[aeiou]/)
     return true
   else
     false
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- 
+
 end
 
 def words_five_letters_long(text)
