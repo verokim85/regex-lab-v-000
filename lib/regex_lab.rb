a860d8ad6ca33c47f0b283f3cc091572eb9f58ef
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 22:31:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b^[A-Z]/) == text.capitalize?
+  if text.match(/\b^[A-Z]/) == text.capitalize!
     true
   end
 end
