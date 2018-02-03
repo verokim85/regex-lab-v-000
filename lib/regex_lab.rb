1965876d104323b96b37d048722229872bef138d
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:36:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.grep(/\A(un)+\z(ing)/)
+  text.grep(/\A(un)+(ing)\z/)
 end
 
 def words_five_letters_long(text)
