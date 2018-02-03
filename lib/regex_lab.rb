d0b8bb523b3accd5f45bb13f7597237f42fe90a5
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:19:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/\A[^aeiou]/)
     return false
-  elsif word.scan(/\A[aeiou]/)
+  else
     return true
   # elsif word.scan(/\A[bcdfghjklmnpqrstvwxyz]/)
 
