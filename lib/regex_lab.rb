a32e935713ebd186c1ea1913cf05b2fb61ae5da0
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 22:57:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\b\W?\d{3}\W?\s?\d{3}\W?\d{4}\b/)
+  if phone.scan(/\W?\d{3}\W?\s?\d{3}\W?\d{4}\b/)
     return true
   else
     return false
