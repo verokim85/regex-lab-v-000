d06dac9361ad4f1223e409442664bb6f0248938c
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:51:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 
   text.each do |word|
       new_array << word.scan(/^(un)+\w+(ing)$/)
-  
+    end
 end
 
 def words_five_letters_long(text)
