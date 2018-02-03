f1e412e6e8c5bddf635f0dd47eefae59d12e067f
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 22:03:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,6 @@
 def starts_with_a_vowel?(word)
-  # if word.scan(/\A[aeiou]/)
-  #   return true
-  # elsif word.scan(/\A[a-zA-Z]^[aeiou])
-  #   false
-  # # elsif word.scan(/\A[bcdfghjklmnpqrstvwxyz]/)
-  # end
+  if \b[aeoiu]
+    return true 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
