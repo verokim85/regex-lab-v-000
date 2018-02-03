1b05fdf16c3f33ba0c10abc95ca419b945f760a1
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:18:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
 def starts_with_a_vowel?(word)
+  if word.scan(/\A[^aeiou]/)
+    return false
+  elsif word.scan(/\A[aeiou]/)
+    return true
+  # elsif word.scan(/\A[bcdfghjklmnpqrstvwxyz]/)
 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\A(un)/) && text.scan(/\z(ing)/)
 end
 
 def words_five_letters_long(text)
-
+ text.match(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
