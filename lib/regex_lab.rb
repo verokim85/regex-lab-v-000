57b6301e61c8e419a12c722d29883f8c87113e39
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 22:27:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z]+\S/)
+  text.match(/\b^[A-Z]+\S\B/)
 end
 
 def valid_phone_number?(phone)
