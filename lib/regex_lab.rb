38d9e3c41b503fcec9d8352cc3471b1a8512d5b0
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 21:33:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,8 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan[/A+(un)\]+[/z+(ing)/]
-end
+ 
 end
 
 def words_five_letters_long(text)
@@ -21,5 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+
 
 end
