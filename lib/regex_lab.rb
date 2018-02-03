e261bae546639328b04a3fa8195b77970d5cfd53
Veronica Kim <vero.a.kim@gmail.com>
2018-02-03 22:17:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[aeoiu]/)
+  if word.scan(/\b[aeoiu]\b/)
     return true
   end
 end
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
